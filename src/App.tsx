f3e78b237e7d43f5a89d8c4c0905e22005b38f2e
gianniresende <gianniresende@gmail.com>
2021-10-21 21:57:58 -0300

    feat: setup switch theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import {StyleSheet} from 'react-native';
 import {ThemeProvider} from 'styled-components';
 import Button from './components/Button';
 import Header from './components/Header';
