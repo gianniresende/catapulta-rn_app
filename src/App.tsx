f54a466e6b94a951d50cf7ea11389e67f48d8921
gianniresende <gianniresende@gmail.com>
2021-11-16 21:03:34 -0300

    feat: initial navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react/self-closing-comp */
 import React, { useRef } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import {Alert, View} from 'react-native';
 import {ThemeProvider} from 'styled-components';
 import Button from '~/components/Button';
@@ -12,6 +13,7 @@ import Icon from '~/components/Icon';
 import Input from '~/components/Input';
 import {InputValueRef} from '~/components/Input/types';
 import Separator from '~/components/Separator';
+
 
 import useAppearence from '~/hooks/Appearence';
 
@@ -28,37 +30,39 @@ const App: React.FC = () => {
   const {theme} = useAppearence();
   return (
     <ThemeProvider theme={theme}>
-      <Container>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            flexWrap: "wrap",
-            }}>
-          <Icon icon="ceifador" size={150}></Icon>
-          <Icon icon="fantasma" size={150}></Icon>
-        </View>
-        <View>
+      <NavigationContainer>
+        <Container>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              flexWrap: "wrap",
+              }}>
+            <Icon icon="ceifador" size={150}></Icon>
+            <Icon icon="fantasma" size={150}></Icon>
+          </View>
+          <View>
+            <Separator height={100} />
+            <Input
+              ref={refEmail}
+              placeholder='jonhdoe@email.com'
+              error=''
+              icon='user'
+              iconPosition='right'
+              label='E-mail'
+            />
+            <Input
+              ref={refPassword}
+              placeholder='Sua senha'
+              secureTextEntry
+              error="user not exist"
+              label='Password'
+            />
+          </View>
           <Separator height={100} />
-          <Input
-            ref={refEmail}
-            placeholder='jonhdoe@email.com'
-            error=''
-            icon='user'
-            iconPosition='right'
-            label='E-mail'
-          />
-          <Input
-            ref={refPassword}
-            placeholder='Sua senha'
-            secureTextEntry
-            error="user not exist"
-            label='Password'
-          />
-        </View>
-        <Separator height={100} />
-        <Button color="error" mode='outlined' onPress={handlePress} loading>Change Icons</Button>
-      </Container>
+          <Button color="error" mode='outlined' onPress={handlePress} loading>Change Icons</Button>
+        </Container>
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
