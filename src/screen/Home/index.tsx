be5d79b641167eaaaf3e8396395dbc6f2239dadf
gianniresende <gianniresende@gmail.com>
2021-11-17 08:19:22 -0300

    feat: passing parameters to routes

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,15 +1,21 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import Text from '~/components/Text';
 import {Container} from './styles';
 
-const Home = ({navigation}) => {
-  const handleNavigation = () => navigation.push('Profile');
+const Home = ({navigation, route}) => {
+  const data = route.params;
+
+  const handleNavigation = () =>
+    navigation.push('Profile', {
+    userName: 'Gianni',
+  });
 
   return (
     <Container>
       <TouchableOpacity onPress={handleNavigation}>
-        <Text>Home</Text>
+        <Text>{`Home: ${data?.selectedUser}`}</Text>
       </TouchableOpacity>
     </Container>
   );
--- src/screen/Profile/index.tsx
+++ src/screen/Profile/index.tsx
@@ -3,17 +3,30 @@ import Text from '~/components/Text';
 import {TouchableOpacity} from 'react-native';
 import {Container} from './styles';
 
-const Profile = ({navigation}) => {
+const Profile = ({navigation, route}) => {
+  const {userName} = route.params;
   const handleNavigation = () => navigation.push('Profile');
-  const handleNavigationToTop = () => navigation.popToTop('Profile');
+
+  const handleUpdateParams = () =>
+    navigation.setParams({
+      userName: 'resendegianni',
+  });
+
+  const handleNavegateToHome = () =>
+    navigation.navigate('Home', {
+      selectedUser: 'resendegianni',
+  });
 
   return (
     <Container>
       <TouchableOpacity onPress={handleNavigation}>
-        <Text>Go back to Home</Text>
+        <Text>{`Profile: ${userName}`}</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleNavigationToTop}>
-        <Text>Go back to Top</Text>
+      <TouchableOpacity onPress={handleUpdateParams}>
+        <Text>Update Params</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleNavegateToHome}>
+        <Text>Navigate to home with params</Text>
       </TouchableOpacity>
     </Container>
   );
