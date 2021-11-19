19f197c58710990a16767bf60e85af939af919c4
gianniresende <gianniresende@gmail.com>
2021-11-19 17:21:52 -0300

    navigate nested navigator

--- src/screen/Feed/index.tsx
+++ src/screen/Feed/index.tsx
@@ -6,9 +6,16 @@ const Feed: React.FC = ({navigation}) => {
     navigation.navigate('stories');
   };
 
+  const handlePressToPosts = () => {
+    navigation.navigate('profile', {
+      screen: 'post',
+    });
+  };
+
   return (
     <View style={{flex: 1, backgroundColor: 'green'}}>
       <Button title="Go to Stories" onPress={handlePress} />
+      <Button title="Go to Posts" onPress={handlePressToPosts} />
     </View>
   );
 };
