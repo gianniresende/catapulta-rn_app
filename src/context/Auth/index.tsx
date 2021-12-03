b8d31ae334f39d6f103b240ad8167b58199b1165
gianniresende <gianniresende@gmail.com>
2021-12-03 07:47:15 -0300

    feat: config async storage

--- src/context/Auth/index.tsx
+++ src/context/Auth/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable quotes */
 /* eslint-disable prettier/prettier */
-import React, {useState, createContext} from "react";
+import React, {useState, useEffect, createContext} from "react";
 import {UserDTO} from "~/@types/dtos/user";
-import {AuthContextProp} from "./types";
+import {asyncUserKeys, AuthContextProp} from "./types";
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext<AuthContextProp>(
   {} as AuthContextProp,
@@ -14,31 +15,53 @@ export const AuthProvider: React.FC = ({children}) => {
   const [user, setUser] = useState<UserDTO>();
   const [loading, setLoading] = useState(false);
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [rehydrateLoading, setrehydrateLoading] = useState(true);
 
   /*
   * Callbacks
   */
 
   const signIn = async ({email, password}: {email: string; password: string;}) => {
-    setLoading(true);
-    const response = await axios.post('http://localhost:8080/api/auth', {
-      email,
-      password,
-    });
-    console.log(response.data.user);
-    setUser(response.data.user);
-    setLoading(false);
-    setIsSignedIn(true);
+    try {
+      setLoading(true);
+      const response = await axios.post('http://localhost:8080/api/auth', {
+        email,
+        password,
+      });
+      console.log(response.data.user);
+      setUser(response.data.user);
+      setLoading(false);
+      setIsSignedIn(true);
+      AsyncStorage.setItem(asyncUserKeys.user, JSON.stringify(response.data.user));
+    } catch (error) {} finally {
+      setLoading(false);
+    }
+
   };
 
-  const signOut = () => {
+  const signOut = async () => {
     setIsSignedIn(false);
     setUser(undefined);
+    await AsyncStorage.clear();
   };
+
+  const rehydrate = async () => {
+    const rehydrateUser = await AsyncStorage.getItem(asyncUserKeys.user);
+
+    if (rehydrateUser) {
+      setUser(JSON.parse(rehydrateUser));
+      setIsSignedIn(true);
+    }
+    setrehydrateLoading(false);
+  };
+
+  useEffect(() => {
+    rehydrate();
+  }, []);
 
   return (
     <AuthContext.Provider value={{user, loading, isSignedIn, signIn, signOut}}>
-      {children}
+      {!rehydrateLoading && children}
     </AuthContext.Provider>
   );
 };
