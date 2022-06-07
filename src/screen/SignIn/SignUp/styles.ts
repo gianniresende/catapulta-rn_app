4fa01bb97362341421756cbbd8e027ad9235a463
gianniresende <gianniresende@gmail.com>
2022-06-07 07:39:10 -0300

    feat: Avoiding addvaccine

--- src/screen/SignIn/SignUp/styles.ts
+++ src/screen/SignIn/SignUp/styles.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import {Pressable} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 import Text from '~/components/Text';
 
--- src/screen/SignedIn/AddVaccineManually/index.tsx
+++ src/screen/SignedIn/AddVaccineManually/index.tsx
@@ -13,6 +13,8 @@ import {Container, ContainerSelect, Scroll, Content} from './styles';
 import {HasSecondShotEnum} from './types';
 import {schemaAddVaccineManually} from './validations';
 import Select from './localComponents/Select';
+import AvoidKeyboard from '~/components/AvoidKeyboard';
+import HeaderOptions from '~/components/HeaderOptions';
 
 const AddVaccineManually = () => {
   const {goBack} = useNavigation();
@@ -25,7 +27,6 @@ const AddVaccineManually = () => {
     control,
     handleSubmit,
     setValue,
-    getValues,
     setFocus,
     formState: {errors},
   } = useForm({
@@ -46,110 +47,31 @@ const AddVaccineManually = () => {
   };
 
   return (
-    <Container>
-      <Scroll>
-        <Content>
-          <StatusBar barStyle={'dark-content'} />
-          <Separator height={spacing.md} />
-          <Pressable>
-            <Icon icon="closeX" size={15} />
-          </Pressable>
-          <Separator height={spacing.md} />
-          <Text typography="h7">Adicione as informações da vacina</Text>
-          <Separator height={spacing.lg} />
-          <Controller
-            control={control}
-            name="name"
-            render={({field: {name, onBlur, onChange, ref, value}}) => (
-              <Input
-                label="Nome da vacina"
-                ref={ref}
-                value={value}
-                onChange={onChange}
-                onBlur={onBlur}
-                onChangeText={text => setValue(name, text)}
-                error={errors[name]?.message}
-                onFocus={() => setFocus(name)}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="brand"
-            render={({field: {name, onBlur, onChange, ref, value}}) => (
-              <Input
-                label="Marca da vacina"
-                ref={ref}
-                value={value}
-                onChange={onChange}
-                onBlur={onBlur}
-                onChangeText={text => setValue(name, text)}
-                error={errors[name]?.message}
-                onFocus={() => setFocus(name)}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="applicationDate"
-            render={({field: {name, onBlur, onChange, ref, value}}) => (
-              <Input
-                label="Data da aplicação"
-                ref={ref}
-                value={value}
-                onChange={onChange}
-                onBlur={onBlur}
-                onChangeText={text => setValue(name, text)}
-                error={errors[name]?.message}
-                onFocus={() => setFocus(name)}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="applicationLocation"
-            render={({field: {name, onBlur, onChange, ref, value}}) => (
-              <Input
-                label="Local da aplicação"
-                ref={ref}
-                value={value}
-                onChange={onChange}
-                onBlur={onBlur}
-                onChangeText={text => setValue(name, text)}
-                error={errors[name]?.message}
-                onFocus={() => setFocus(name)}
-              />
-            )}
-          />
-          <Separator height={spacing.md} />
-          <Text color="surface600" typography="body3">
-            Possui segunda doze?
-          </Text>
-          <Separator height={spacing.sm} />
-          <ContainerSelect>
-            <Select
-              onPress={() => setHasSecondShot(HasSecondShotEnum.YES)}
-              isSelected={hasSecondShot === HasSecondShotEnum.YES}
-              title="Sim"
+    <AvoidKeyboard>
+      <Container>
+        <Scroll>
+          <Content>
+            <StatusBar
+              barStyle="dark-content"
+              translucent
+              backgroundColor={'transparent'}
             />
-            <Select
-              onPress={() => setHasSecondShot(HasSecondShotEnum.NO)}
-              isSelected={hasSecondShot === HasSecondShotEnum.NO}
-              title="Não"
+            <HeaderOptions
+              left={
+                <Pressable onPress={goBack}>
+                  <Icon icon="closeX" size={15} />
+                </Pressable>
+              }
             />
-            <Select
-              onPress={() => setHasSecondShot(HasSecondShotEnum.SINGLE)}
-              isSelected={hasSecondShot === HasSecondShotEnum.SINGLE}
-              title="Dose única"
-            />
-          </ContainerSelect>
-          {hasSecondShot === HasSecondShotEnum.YES && (
+            <Separator height={spacing.md} />
+            <Text typography="h7">Adicione as informações da vacina</Text>
+            <Separator height={spacing.lg} />
             <Controller
               control={control}
-              name="nextApplicationDate"
+              name="name"
               render={({field: {name, onBlur, onChange, ref, value}}) => (
                 <Input
-                  label="Data da próxima aplicação"
+                  label="Nome da vacina"
                   ref={ref}
                   value={value}
                   onChange={onChange}
@@ -157,16 +79,113 @@ const AddVaccineManually = () => {
                   onChangeText={text => setValue(name, text)}
                   error={errors[name]?.message}
                   onFocus={() => setFocus(name)}
+                  returnKeyType="next"
+                  onSubmitEditing={() => setFocus('brand')}
                 />
               )}
             />
-          )}
-          <Separator height={spacing.md} />
-          <Button onPress={onSubmit}>Salvar</Button>
-          <Separator height={spacing.lg} />
-        </Content>
-      </Scroll>
-    </Container>
+            <Controller
+              control={control}
+              name="brand"
+              render={({field: {name, onBlur, onChange, ref, value}}) => (
+                <Input
+                  label="Marca da vacina"
+                  ref={ref}
+                  value={value}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  onChangeText={text => setValue(name, text)}
+                  error={errors[name]?.message}
+                  onFocus={() => setFocus(name)}
+                  returnKeyType="next"
+                  onSubmitEditing={() => setFocus('applicationDate')}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="applicationDate"
+              render={({field: {name, onBlur, onChange, ref, value}}) => (
+                <Input
+                  label="Data da aplicação"
+                  ref={ref}
+                  value={value}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  onChangeText={text => setValue(name, text)}
+                  error={errors[name]?.message}
+                  onFocus={() => setFocus(name)}
+                  returnKeyType="next"
+                  onSubmitEditing={() => setFocus('applicationLocation')}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="applicationLocation"
+              render={({field: {name, onBlur, onChange, ref, value}}) => (
+                <Input
+                  label="Local da aplicação"
+                  ref={ref}
+                  value={value}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  onChangeText={text => setValue(name, text)}
+                  error={errors[name]?.message}
+                  onFocus={() => setFocus(name)}
+                  returnKeyType="next"
+                />
+              )}
+            />
+            <Separator height={spacing.md} />
+            <Text color="surface600" typography="body3">
+              Possui segunda doze?
+            </Text>
+            <Separator height={spacing.sm} />
+            <ContainerSelect>
+              <Select
+                onPress={() => setHasSecondShot(HasSecondShotEnum.YES)}
+                isSelected={hasSecondShot === HasSecondShotEnum.YES}
+                title="Sim"
+              />
+              <Select
+                onPress={() => setHasSecondShot(HasSecondShotEnum.NO)}
+                isSelected={hasSecondShot === HasSecondShotEnum.NO}
+                title="Não"
+              />
+              <Select
+                onPress={() => setHasSecondShot(HasSecondShotEnum.SINGLE)}
+                isSelected={hasSecondShot === HasSecondShotEnum.SINGLE}
+                title="Dose única"
+              />
+            </ContainerSelect>
+            {hasSecondShot === HasSecondShotEnum.YES && (
+              <Controller
+                control={control}
+                name="nextApplicationDate"
+                render={({field: {name, onBlur, onChange, ref, value}}) => (
+                  <Input
+                    label="Data da próxima aplicação"
+                    ref={ref}
+                    value={value}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    onChangeText={text => setValue(name, text)}
+                    error={errors[name]?.message}
+                    onFocus={() => setFocus(name)}
+                    returnKeyType="done"
+                    onSubmitEditing={onSubmit}
+                  />
+                )}
+              />
+            )}
+            <Separator height={spacing.md} />
+            <Button onPress={onSubmit}>Salvar</Button>
+            <Separator height={spacing.lg} />
+          </Content>
+        </Scroll>
+      </Container>
+    </AvoidKeyboard>
   );
 };
 
